667208e50b0f3fec90ca7cda24f610045e661a35
Olenka Vargas Lazarte <olenka@ravn.co>
2021-01-07 19:35:59 -0500

    feat: chap4.2

--- chapter4.js
+++ chapter4.js
@@ -1,6 +1,5 @@
 //Chapter 3:
 //////////////////////////////////////////
-// Your code here.
 function range(start, end, step=1)
 {
   let list = [], tmp = start;
@@ -32,6 +31,30 @@ console.log(sum(range(1, 10)));
 
 //////////////////////////////////////////
 
+function reverseArray(array)
+{
+  let newArray = [];
+  for(let i=array.length-1; i>=0; i--)
+    newArray.push(array[i]);
+  return newArray;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) // takes the middle like a base
+  { 
+    let old = array[i]; // temp variable first
+    array[i] = array[array.length - 1 - i]; //switch first by last
+    array[array.length - 1 - i] = old; // switch last by temp, so first
+  }
+  return array; // is necessary return the input parameter
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
 
 
 //////////////////////////////////////////
