54ad70a4a7ab8081f5a34a11d5bd86bf137ef0ac
Olenka Vargas Lazarte <olenka@ravn.co>
2021-01-08 16:52:26 -0500

    feat: chap5.4

--- chapter5.js
+++ chapter5.js
@@ -40,3 +40,15 @@ console.log(every([], n => n < 10));
 
 //////////////////////////////////////////
 
+function dominantDirection(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
+  return scripts;
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
