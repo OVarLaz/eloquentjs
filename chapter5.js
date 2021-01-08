b948bfca19ed63de7ba1aca752e4515b68c73cc1
Olenka Vargas Lazarte <olenka@ravn.co>
2021-01-08 15:40:58 -0500

    feat: chap5.3

--- chapter5.js
+++ chapter5.js
@@ -23,7 +23,20 @@ loop(3, n => n > 0, n => n - 1, console.log);
 
 //////////////////////////////////////////
 
+function every(array, test) {
+  for(val of array)
+    if(!test(val)) return false;
+  return true;
+}
+console.log(([2, 4, 16].map( n => n < 10)).reduce((n,m) => n===m));
+//no works with empty array
 
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
 
 //////////////////////////////////////////
 
